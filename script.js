22f2b59018092ea0198e42c1190ec9be612d8e98
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-08-25 13:51:20 +0300

    visible animation

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ var x = 0;
 function draw() {
 	window.requestAnimationFrame(draw);
 	ctx.clearRect(0, 0, 800, 600);
+
+	ctx.fillStyle = 'rgba(150, 150, 150, 0.5)';
 	ctx.fillRect(x, 20, 150, 100);
 	x += 5;
 	if (x > 800) {
-		x %= 800;
+		x = -100;
 	}
 }
 
