a50e6e0f407bb2fda1a6bbc16758b60870ee32c4
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-08-25 13:50:06 +0300

    animate

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 var c = document.getElementById("game_canvas");
 var ctx = c.getContext("2d");
-ctx.fillRect(20, 20, 150, 100);
+
+var x = 0;
+
+function draw() {
+	window.requestAnimationFrame(draw);
+	ctx.clearRect(0, 0, 800, 600);
+	ctx.fillRect(x, 20, 150, 100);
+	x += 5;
+	if (x > 800) {
+		x %= 800;
+	}
+}
+
+window.requestAnimationFrame(draw);
