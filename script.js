e340b68ae4ebbb754488a839efd777a77e39649a
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-08-25 14:02:24 +0300

    some blocks

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+var width = 960;
+var height = 540;
+
+var blocks = [
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 2, 2, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 1, 1, 1, 1, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+var colors = [
+	null,
+	'rgba(240, 240, 240, 1.0)',
+	'rgba(240, 120, 0, 1.0)',
+	'rgba(0, 120, 240, 1.0)',
+];
+
 var c = document.getElementById("game_canvas");
 var ctx = c.getContext("2d");
 
@@ -5,10 +28,16 @@ var x = 0;
 
 function draw() {
 	window.requestAnimationFrame(draw);
-	ctx.clearRect(0, 0, 800, 600);
+	ctx.clearRect(0, 0, width, height);
 
-	ctx.fillStyle = 'rgba(150, 150, 150, 0.5)';
-	ctx.fillRect(x, 20, 150, 100);
+	for (var y = 0; y < 10; ++y) {
+		for (var x = 0; x < 10; ++x) {
+			if (blocks[y][x]) {
+				ctx.fillStyle = colors[blocks[y][x]];
+				ctx.fillRect(x * 32, y * 32, 32, 32);
+			}
+		}
+	}
 	x += 5;
 	if (x > 800) {
 		x = -100;
